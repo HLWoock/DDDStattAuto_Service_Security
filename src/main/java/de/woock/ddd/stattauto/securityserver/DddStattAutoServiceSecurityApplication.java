0dbbf3f74c369f0489eba26709e00c85fb5b4c5c
HermannW <HermannW@Noose-1099.oosede.local>
2017-06-12 22:44:10 +0200

    initial

---
+++ src/main/java/de/woock/ddd/stattauto/securityserver/DddStattAutoServiceSecurityApplication.java
@@ -0,0 +1,12 @@
+package de.woock.ddd.stattauto.securityserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DddStattAutoServiceSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DddStattAutoServiceSecurityApplication.class, args);
+	}
+}
---
+++ src/test/java/de/woock/ddd/stattauto/securityserver/DddStattAutoServiceSecurityApplicationTests.java
@@ -0,0 +1,16 @@
+package de.woock.ddd.stattauto.securityserver;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DddStattAutoServiceSecurityApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
